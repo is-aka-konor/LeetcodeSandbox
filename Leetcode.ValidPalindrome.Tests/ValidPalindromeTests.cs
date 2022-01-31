ab5dd76931c9521b81eb509b26d3d78f02fd4c4d
Sergey Idelson <aka.konor@gmail.com>
2022-01-31 15:40:45 +1300

    Added IsPalindrome solution and tests

---
+++ Leetcode.ValidPalindrome.Tests/ValidPalindromeTests.cs
@@ -0,0 +1,49 @@
+﻿using Xunit;
+
+namespace Leetcode.ValidPalindrome.Tests;
+
+public class ValidPalindromeInput
+{
+    public string Input { get; set; } = string.Empty;
+    public bool Result { get; set; }
+}
+public class ValidPalindromeTests
+{
+    [Theory]
+    [MemberData(nameof(IsValidPalindromeData), MemberType = typeof(ValidPalindromeTests))]
+    public void SohuldPassValidPalindromeTests(ValidPalindromeInput data)
+    {
+        var solutionImplementation = new Solution();
+        var targetResult = solutionImplementation.IsPalindrome(data.Input);
+        Assert.Equal(data.Result, targetResult);
+    }
+
+    public static TheoryData<ValidPalindromeInput> IsValidPalindromeData =>
+        new TheoryData<ValidPalindromeInput>
+        {
+            new ValidPalindromeInput {
+                Input = " ",
+                Result = true
+            },
+            new ValidPalindromeInput {
+                Input = " WoW ",
+                Result = true
+            },
+            new ValidPalindromeInput {
+                Input = " ,121! ",
+                Result = true
+            },
+            new ValidPalindromeInput {
+                Input = "A man, a plan, a canal: Panama",
+                Result = true
+            },
+            new ValidPalindromeInput {
+                Input = "race a car",
+                Result = false
+            },
+            new ValidPalindromeInput {
+                Input = "!Miro !Miro",
+                Result = false
+            },
+        };
+}
---
+++ Leetcode.ValidPalindrome/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Leetcode.ValidPalindrome
+{
+    public class Solution
+    {
+        public bool IsPalindrome(string s)
+        {
+            var cleanString = s.ToLower().Where(ch => char.IsLetterOrDigit(ch)).ToArray();
+            if (cleanString.Length == 0) return true;
+
+            var steps = cleanString.Count() / 2;
+            for (var i = 0; i <= steps; ++i)
+            {
+                if(cleanString[i] != cleanString[cleanString.Length - 1 - i])
+                return false;
+            }
+            return true;
+        }
+    }
+}
+
