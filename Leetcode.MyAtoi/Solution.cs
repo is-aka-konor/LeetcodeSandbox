2c4c3d0359b2d092bf70007850606ad8fd42dfab
Sergey Idelson <aka.konor@gmail.com>
2022-02-02 17:14:00 +1300

    Speed the things up a bit

--- Leetcode.MyAtoi/Solution.cs
+++ Leetcode.MyAtoi/Solution.cs
@@ -3,72 +3,73 @@ using System.Text;
 
 namespace Leetcode.MyAtoi
 {
-	public class Solution
-	{
-		public int MyAtoi(string s)
-		{
-			var cleanString = s.TrimStart();
-			if (!(cleanString.Length > 0)
-				|| (cleanString.Length == 1 && !Char.IsDigit(cleanString[0])))
-			{
-				return 0;
-			}
-			int index = 0;
-			var length = cleanString.Length;
-			// Search for the first digit or a sign symbol
-			if (index < length
-				&& !Char.IsDigit(cleanString[index])
-				&& cleanString[index] != '-'
-				&& cleanString[index] != '+')
-            {
-				return 0;
-            }
-			// Adjust start position if starts with minus
-			int multiplyer;
-			if (cleanString[index] == '-')
-            {
-				multiplyer = -1;
-				++index;
-            }
-            else if(cleanString[index] == '+')
-            {
-				multiplyer = 1;
-				++index;
-				
-			}
-			else
-			{
-				multiplyer = 1;
-            }
-			if (index < length
-				&& !Char.IsDigit(cleanString[index])) {
-				return 0;
-			}
-			
-			while(index < length
-					&& cleanString[index] == '0')
-            {
-				++index;
-            };
-			var result = new StringBuilder();
-			while (index < length
-				&& Char.IsDigit(cleanString[index]))
-            {
-				result.Append(cleanString[index]);
-				++index;
-            }
-			var stringResult = result.ToString();
-			if (stringResult.Length > 10)
-            {
-				return multiplyer > 0 ? int.MaxValue : Int32.MinValue;
-            }
-			if (result.Length == 0) return 0;
-			var number = Convert.ToInt64(stringResult) * multiplyer;
+  public class Solution
+  {
+    public int MyAtoi(string s)
+    {
+      var cleanString = s.TrimStart();
+      if (!(cleanString.Length > 0)
+        || (cleanString.Length == 1 && !Char.IsDigit(cleanString[0])))
+      {
+        return 0;
+      }
+      int index = 0;
+      var length = cleanString.Length;
+      // Search for the first digit or a sign symbol
+      if (index < length
+        && !Char.IsDigit(cleanString[index])
+        && cleanString[index] != '-'
+        && cleanString[index] != '+')
+      {
+        return 0;
+      }
+      // Adjust start position if starts with minus
+      int multiplyer;
+      if (cleanString[index] == '-')
+      {
+        multiplyer = -1;
+        ++index;
+      }
+      else if (cleanString[index] == '+')
+      {
+        multiplyer = 1;
+        ++index;
 
-			if (number > Int32.MaxValue) return Int32.MaxValue;
-			else if (number < Int32.MinValue) return Int32.MinValue;
-			else return (int)number;
-		}
-	}
+      }
+      else
+      {
+        multiplyer = 1;
+      }
+      if (index < length
+        && !Char.IsDigit(cleanString[index]))
+      {
+        return 0;
+      }
+
+      while (index < length
+          && cleanString[index] == '0')
+      {
+        ++index;
+      };
+      var startingIndex = index;
+      while (index < length
+        && Char.IsDigit(cleanString[index]))
+      {
+        ++index;
+      }
+      var stringResult = cleanString.Substring(startingIndex, index - startingIndex);
+
+      if (stringResult.Length > 10)
+      {
+        return multiplyer > 0 ? int.MaxValue : Int32.MinValue;
+      }
+      if (stringResult.Length == 0) return 0;
+      var number = Convert.ToInt64(stringResult) * multiplyer;
+
+      if (number > Int32.MaxValue) return Int32.MaxValue;
+      else if (number < Int32.MinValue) return Int32.MinValue;
+      else return (int)number;
+    }
+  }
 }
 
