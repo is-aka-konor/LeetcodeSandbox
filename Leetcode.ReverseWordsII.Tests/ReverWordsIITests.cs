3928c1d23534acd6e5ef7aa0fa0621a847db4255
Sergey Idelson <aka.konor@gmail.com>
2022-02-04 20:37:21 +1300

    Added solution and tests for ReverseWordsII

---
+++ Leetcode.ReverseWordsII.Tests/ReverWordsIITests.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+using System;
+
+namespace Leetcode.ReverseWordsII.Tests
+{
+    public class ReverseWordsIITest
+    {
+        [Theory]
+        [InlineData("the sky is blue", "blue is sky the")]
+        [InlineData("a", "a")]
+        [InlineData("hello world", "world hello")]
+        [InlineData("a good example", "example good a")]
+        public void ShouldPassReverseWordsTests(string input, string output)
+        {
+            var solution = new Solution();
+            var target = solution.ReverseWords(input.ToCharArray());
+            Assert.Equal(output.ToCharArray(), target);
+        }
+    }
+}
---
+++ Leetcode.ReverseWordsII/Solution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace Leetcode.ReverseWordsII
+{
+	public class Solution
+	{
+		public char[] ReverseWords(char[] s)
+        {
+			var stack = new Stack<char[]>();
+			var buffer = new List<char>();
+			for(var i = 0; i < s.Length; ++i)
+            {
+				if(s[i] != ' ')
+				{
+					buffer.Add(s[i]);
+				}else if(s[i] == ' ')
+				{
+					stack.Push(buffer.ToArray());
+					buffer = new List<char>();
+				}
+            }
+			stack.Push(buffer.ToArray());
+			var index = 0;
+			var wordsCount = stack.Count;
+			for(var i = 0; i < wordsCount; ++i)
+			{
+				var word = stack.Pop();
+				for(var j = 0; j < word.Length; ++j)
+				{
+					s[index] = word[j];
+					index++;
+				}
+				if(stack.Count != 0)
+				{
+					s[index] = ' ';
+					index++;
+				}
+			}
+			return s;
+		}
+	}
+}
