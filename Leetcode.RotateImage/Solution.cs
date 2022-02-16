9d2b2213fd2b4a5d0185d1240f1e713d7d1d337c
Sergey Idelson <aka.konor@gmail.com>
2022-02-16 21:16:43 +1300

    Added rotate image solution

--- Leetcode.RotateImage/Solution.cs
+++ Leetcode.RotateImage/Solution.cs
@@ -2,7 +2,24 @@
 
 public class Solution
 {
-    public void Rotate(int[][] matrix) {
-        
+    public void Rotate(int[][] matrix)
+    {
+        var left = 0;
+        var right = matrix.Length - 1;
+        while (left < right)
+        {
+            for (var i = 0; i < right-left; ++i)
+            {
+                var top = left;
+                var bottom = right;
+                var topLeft = matrix[top][left + i];
+                matrix[top][left + i] = matrix[bottom - i][left];
+                matrix[bottom - i][left] = matrix[bottom][right - i];
+                matrix[bottom][right - i] = matrix[top + i][right];
+                matrix[top + i][right] = topLeft;
+            }
+            ++left;
+            --right;
+        }
     }
 }
