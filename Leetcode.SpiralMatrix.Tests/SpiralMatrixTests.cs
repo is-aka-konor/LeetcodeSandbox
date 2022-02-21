01b55fec07bf4bc2320eef5c18ae3a3e415b582d
Sergey Idelson <aka.konor@gmail.com>
2022-02-21 19:38:03 +1300

    Added Spiral Matrix test project

--- Leetcode.SpiralMatrix.Tests/SpiralMatrixTests.cs
+++ Leetcode.SpiralMatrix.Tests/SpiralMatrixTests.cs
@@ -1,11 +1,48 @@
+using System.Collections.Generic;
 using Xunit;
 
 namespace Leetcode.SpiralMatrix.Tests;
 
-public class UnitTest1
+public class SpiralMatrixTestInput
 {
-    [Fact]
-    public void Test1()
+    public int[][] Input { get; set; }
+    public IList<int> Output { get; set; }
+
+}
+
+public class SpiralMatrixTests
+{
+    [Theory]
+    [MemberData(nameof(SpiralMatrixTestData), MemberType = typeof(SpiralMatrixTests))]
+    public void ShouldPassSpiralMatrixTests(SpiralMatrixTestInput input)
     {
+        var spiralMatrix = new Solution();
+        var result = spiralMatrix.SpiralOrder(input.Input);
+        Assert.Equal(input.Output, result);
     }
+    
+    public static TheoryData<SpiralMatrixTestInput> SpiralMatrixTestData =>
+        new TheoryData<SpiralMatrixTestInput>
+        {
+            new SpiralMatrixTestInput
+            {
+                Input = new int[][]
+                {
+                    new int[] { 1, 2, 3 },
+                    new int[] { 4, 5, 6 },
+                    new int[] { 7, 8, 9 }
+                },
+                Output = new List<int> { 1, 2, 3, 6, 9, 8, 7, 4, 5 }
+            },
+            new SpiralMatrixTestInput()
+            {
+                Input = new int[][]
+                {
+                    new int[] { 1, 2, 3, 4 },
+                    new int[] { 5, 6, 7, 8 },
+                    new int[] { 9, 10, 11, 12 }
+                },
+                Output = new List<int> { 1,2,3,4,8,12,11,10,9,5,6,7 }
+            }
+        };
 }
